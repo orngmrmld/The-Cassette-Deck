bba6c0f28b1e7946ad73189116908970eea08436
jdeino <josh.deino91@yahoo.com>
2023-08-23 19:55:27 -0700

    Adding home-page

---
+++ src/components/app.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './App.css';
+import Header from './Header';
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <main>
+        <p>Welcome to 'The Cassette Deck' social platform.</p>
+        {/* Add more content here */}
+      </main>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/header.js
@@ -0,0 +1,20 @@
+// Header.js
+import React from 'react';
+import './header.css';
+
+function Header() {
+  return (
+    <header className="header">
+      <h1 className="graffiti-title">The Cassette Deck</h1>
+      <nav className="nav">
+        <ul>
+          <li>Home</li>
+          <li>Friends</li>
+          <li>Profile</li>
+        </ul>
+      </nav>
+    </header>
+  );
+}
+
+export default Header;
