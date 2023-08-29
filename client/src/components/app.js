077878f693d3f5170a200427fc8723298f842702
Gustavo <diazgus411@gmail.com>
2023-08-28 19:49:02 -0700

    test for back end

--- client/src/components/app.js
+++ client/src/components/app.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import './App.css';
 import Header from './Header';
+import CommentBubble from './commentbubble'; // Adjust the path based on your folder structure
 
 function App() {
   return (
@@ -8,6 +9,11 @@ function App() {
       <Header />
       <main>
         <p>Welcome to 'The Cassette Deck' social platform.</p>
+        
+        <div className="comments-section">
+          <CommentBubble author="John" text="Great post!" />
+          <CommentBubble author="Alice" text="I agree with you." />
+        </div>
         {/* Add more content here */}
       </main>
     </div>
