ea0f836169883e6f2d2f380b35b3f4a011e855e0
Gustavo <diazgus411@gmail.com>
2023-08-29 21:46:25 -0700

    random tests

--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -1,38 +1,56 @@
-// import { gql } from '@apollo/client';
+import { gql } from '@apollo/client';
 
-// export const LOGIN_USER = gql`
-// mutation login($email: String!, $password: String!) {
-//     login(email: $email, password: $password) {
-//         token
-//         user {
-//             _id
-//             username
-//         }
-//     }
-//   } 
-// `;
+export const LOGIN_USER = gql`
+  mutation login($email: String!, $password: String!) {
+    login(email: $email, password: $password) {
+      token
+      user {
+        _id
+        username
+      }
+    }
+  }
+`;
 
-// export const ADD_USER = gql`
-//     mutation addUser($username: String!, $email: String!, $password)
-//         addUser(username: $username, email: $email, password: $password) {
-//             token
-//             user {
-//                 _id
-//                 username
-//             }
-//         }
-// `;
+export const ADD_USER = gql`
+  mutation addUser($username: String!, $email: String!, $password: String!) {
+    addUser(username: $username, email: $email, password: $password) {
+      token
+      user {
+        _id
+        username
+      }
+    }
+  }
+`;
 
-// export const POST_BLOG = gql`
-//   mutation ($name: String!, $post: String!) {
+export const ADD_THOUGHT = gql`
+  mutation addThought($thoughtText: String!) {
+    addThought(thoughtText: $thoughtText) {
+      _id
+      thoughtText
+      thoughtAuthor
+      createdAt
+      comments {
+        _id
+        commentText
+      }
+    }
+  }
+`;
 
-//     }
-// `;
-
-// export const ADD_COMMENT = gql`
-//   mutation ($_id: String!, $techNum: Int!) {
-    
-   
-//     }
-
-// `;
+export const ADD_COMMENT = gql`
+  mutation addComment($thoughtId: ID!, $commentText: String!) {
+    addComment(thoughtId: $thoughtId, commentText: $commentText) {
+      _id
+      thoughtText
+      thoughtAuthor
+      createdAt
+      comments {
+        _id
+        commentText
+        createdAt
+      }
+    }
+  }
+`;
--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -2,25 +2,48 @@ const { gql } = require('apollo-server-express');
 
 const typeDefs = gql`
   type User {
-    _id: ID!
-    name: String!
+    _id: ID
+    username: String
     email: String
+    password: String
+    thoughts: [Thought]!
   }
 
-  type Post {
-    _id: ID!
-
+  type Thought {
+    _id: ID
+    thoughtText: String
+    thoughtAuthor: String
+    createdAt: String
+    comments: [Comment]!
   }
- 
+
   type Comment {
-    _id: ID!
+    _id: ID
+    commentText: String
+    commentAuthor: String
+    createdAt: String
+  }
+
+  type Auth {
+    token: ID!
+    user: User
   }
 
   type Query {
-    user: [User]
-    post: [Post]
-    comment: [Comment]
-    
+    users: [User]
+    user(username: String!): User
+    thoughts(username: String): [Thought]
+    thought(thoughtId: ID!): Thought
+    me: User
+  }
+
+  type Mutation {
+    addUser(username: String!, email: String!, password: String!): Auth
+    login(email: String!, password: String!): Auth
+    addThought(thoughtText: String!): Thought
+    addComment(thoughtId: ID!, commentText: String!): Thought
+    removeThought(thoughtId: ID!): Thought
+    removeComment(thoughtId: ID!, commentId: ID!): Thought
   }
 `;
 
