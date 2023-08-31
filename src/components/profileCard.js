107cac2f271f6a969a07906f69cc2196ecabe801
jdeino <josh.deino91@yahoo.com>
2023-08-30 20:58:19 -0700

    adding new style to homepage

---
+++ src/components/profileCard.js
@@ -0,0 +1,42 @@
+import React from 'react';
+import './profileCard.css'; 
+
+function ProfileCard({ user, playlist }) {
+  return (
+    <div className="profile-card">
+      {/* Display user's name */}
+      <h2 className="user-name">{user.name}</h2>
+
+      {/* User's picture container */}
+      <div className="user-picture-container">
+        <img src={user.avatar} alt={user.name} className="user-picture" />
+      </div>
+
+      {/* Vibe meter */}
+      <div className="vibe-meter">
+        {/* Labels for "Whack" and "Sick" */}
+        <div className="vibe-label left">Whack</div>
+        <input type="range" min="0" max="100" step="1" />
+        <div className="vibe-label right">Sick</div>
+      </div>
+
+      {/* Display user's Spotify playlist */}
+      <div className="playlist">
+        <h3>Spotify Playlist</h3>
+        {/* Display the playlist */}
+        {/* You can customize how you want to show the playlist */}
+      </div>
+
+      {/* Comment section */}
+      <div className="comment-section">
+        <textarea
+          className="comment-input"
+          placeholder="Add a comment..."
+        ></textarea>
+        <button className="comment-button">Comment</button>
+      </div>
+    </div>
+  );
+}
+
+export default ProfileCard;
